89502e1fed9cbb423267edaa9744ebdb7128945c
Ankit Kharbanda <nextgen.erdev@gmail.com>
2026-09-14 21:51:24 +0530

    Simple promise creation

---
+++ callbacksAndEvents/promisesAsyncAwait/examples/delay.mjs
@@ -0,0 +1,11 @@
+export function delay(milliseconds) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(new Date())
+        }, milliseconds)
+    })
+}
+
+
+console.log(`Delaying ... ${new Date().getSeconds()}s`)
+delay(5000).then(newDate => console.log(`Done... ${newDate.getSeconds()}s`))
