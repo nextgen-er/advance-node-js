84ce4a9643efee1e72cf05e91b6e21513ff96624
Ankit Kharbanda <nextgen.erdev@gmail.com>
2026-09-14 15:25:51 +0530

    Added TaskQueue for Queue Crawling

--- utils/spider.mjs
+++ utils/spider.mjs
@@ -5,6 +5,8 @@ import slug from 'slug'
 import superagent from 'superagent'
 import { mkdirp } from 'mkdirp'
 import * as cheerio from 'cheerio'
+import { EventEmitter } from 'events'
+
 
 
 export function urlToFilename(url) {
@@ -70,3 +72,36 @@ export function getPageLinks(currentUrl, body) {
         })
         .filter(Boolean)
 };
+
+export class TaskQueue extends EventEmitter {
+    constructor(concurrency) {
+        super()
+        this.concurrency = concurrency
+        this.running = 0
+        this.queue = []
+    }
+
+    pushTask(task) {
+        this.queue.push(task)
+        process.nextTick(this.next.bind(this))
+        return this
+    }
+
+    next() {
+        if (this.running === 0 && this.queue.length === 0) {
+            return this.emit('empty')
+        }
+
+        while (this.running < this.concurrency && this.queue.length) {
+            const task = this.queue.shift()
+            task((err) => {
+                if (err) {
+                    this.emit('error', err)
+                }
+                this.running--
+                process.nextTick(this.next.bind(this))
+            })
+            this.running++
+        }
+    }
+}
